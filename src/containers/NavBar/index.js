c8a2353fe609ae6e850ac0977083f5e82bee7cd3
Tecode <283731869@qq.com>
2018-06-09 10:35:57 +0800

    'modify_scss'

--- src/containers/NavBar/index.js
+++ src/containers/NavBar/index.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import {Helmet} from 'react-helmet';
+import styles from './index.scss';
 
 export default class NavBar extends Component {
   render() {
     return (
       <div>
         <Helmet defaultTitle="文章相关" />
-        文章相关56
+        <p className={styles.title}>文章相关56</p>
       </div>
     );
   }

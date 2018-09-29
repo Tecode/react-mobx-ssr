0a314eee6f2ee16d3022b28093ee8dfdb67491f3
谢明 <ming.xie@socialcredits.cn>
2018-09-29 14:32:05 +0800

    fix: 解决报错警告

--- src/helpers/Html.js
+++ src/helpers/Html.js
@@ -8,7 +8,7 @@ const Html = ({ componentHTML, initData }) => {
   return (
     <html>
       <head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
         <meta name="mobile-web-app-capable" content="yes" />
